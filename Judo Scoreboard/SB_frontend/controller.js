6bc45a9f03af5fc927256921ed2c78a526e3478a
Nictec <nicnac9911@gmail.com>
2017-04-17 21:49:35 +0200

    added settings modal

--- Judo Scoreboard/SB_frontend/controller.js
+++ Judo Scoreboard/SB_frontend/controller.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8000');
+const socket = new WebSocket('ws://192.168.0.106:8000');
 var connected
 socket.addEventListener('open', function (event) {
 	connected = true
@@ -127,4 +127,35 @@ function resetAll(){
 	document.getElementById('ipDisp2').innerHTML = 0; 
 	document.getElementById('wazaDisp2').innerHTML = 0; 
 	document.getElementById('shiDisp2').innerHTML = 0;     
+}
+
+function toggle(){
+	socket.send('time.toggle');
+}
+
+//modal
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
